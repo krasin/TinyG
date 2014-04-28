7d65392aba3550053815fab98e7791bcbcc586bb
Ivan Krasin <imkrasin@gmail.com>
2014-04-27 17:29:22 -0700

    simulator: return correct exit code if some tests failed

--- firmware/tinyg/sim/test.sh
+++ firmware/tinyg/sim/test.sh
@@ -6,17 +6,20 @@
 set -o nounset
 set -o errexit
 
+EXIT_CODE=0
+
+fail() {
+  EXIT_CODE=1
+  echo "FAILED"
+}
+
 echo "============================================="
-echo "Running very basic test with full output ..."
+echo "Running very basic test with full checks ..."
 echo "============================================="
 echo ""
 
-./tinyg.elf < test.gcode
-
-echo "============================================="
-echo "OK"
-echo "============================================="
-echo ""
+echo -n "test.gcode -- "
+( ./tinyg.elf test.gcode && echo "OK" ) || fail
 
 echo ""
 echo "============================================="
@@ -28,5 +31,15 @@ for i in ../../../gcode_samples/*.gcode
 do
   echo -n "$i -- "
   OUT_FILE=`basename $i`.out
-  (( ./tinyg.elf $i > $OUT_FILE 2>&1 ) && echo " OK" ) || ( echo " FAIL" && cat $OUT_FILE )
+  (( ./tinyg.elf $i > $OUT_FILE 2>&1 ) && echo "OK" ) || ( fail && cat $OUT_FILE )
 done
+
+echo ""
+if [ $EXIT_CODE -eq 0 ];
+then
+  echo "All tests PASSED"
+else
+  echo "Some tests FAILED"
+fi
+
+exit $EXIT_CODE
